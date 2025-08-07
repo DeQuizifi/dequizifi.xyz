fe669c8c7aa384450283dc2d2050973ddbbeacd1
Hala Jeet <halajeetweb3@gmail.com>
2025-08-06 22:04:00 -0600

    Add Header component: Implement Header with title and optional subtitle for improved UI structure

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,27 @@
+"use client";
+type HeaderProps = {
+    title: string;
+    subtitle?: string;
+}  
+
+export default function Header({title,subtitle}: HeaderProps) {
+    return (
+        <div className="bg-sidebar p-4">
+            <header>
+                <h1 className="
+                text-5xl 
+                font-bold 
+                text-center 
+                bg-gradient-to-r
+                from-ring 
+                to-popover 
+                bg-clip-text 
+                text-transparent 
+                mb-3
+  ">{title}</h1>
+                {subtitle && 
+                <p className="text-xl text-center text-chart-5 font-sans">{subtitle}</p>}
+            </header>
+        </div>
+    )
+}
