0c88099994dfb03f31b686d7313b72e9d8cffdad
Lance Herman <lance.herman.btc@gmail.com>
2025-08-11 01:25:03 -0600

    Add Footer Component to Home Page for Enhanced UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-
-import Hero from '@/components/Section/Hero/Hero';
-import Feature from '../components/Section/Feature/Feature';
+import Hero from "@/components/Section/Hero/Hero";
+import Feature from "../components/Section/Feature/Feature";
+import Footer from "@/components/Section/Footer/footer";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Feature/>
+      <Feature />
+      <Footer />
     </main>
   );
 }
