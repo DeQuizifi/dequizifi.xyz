7a7e7fbb38c5581d7501d3da210525d32913d6a5
Lance Herman <lance.herman.btc@gmail.com>
2025-08-11 01:35:05 -0600

    make Footer component available on whole website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Hero from "@/components/Section/Hero/Hero";
 import Feature from "../components/Section/Feature/Feature";
-import Footer from "@/components/Section/Footer/footer";
+
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Feature />
-      <Footer />
     </main>
   );
 }
