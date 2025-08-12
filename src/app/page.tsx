cc9dc0fbf2f59d32fa1c772a198280b39552b4cf
Lance Herman <lance.herman.btc@gmail.com>
2025-08-11 21:55:49 -0600

    feat: add footer component to home page  (#23)
    * Add Footer Component to Home Page for Enhanced UI
    
    * make Footer component available on whole website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import Hero from "@/components/Section/Hero/Hero";
+import Feature from "../components/Section/Feature/Feature";
 
-import Hero from '@/components/Section/Hero/Hero';
-import Feature from '../components/Section/Feature/Feature';
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Feature/>
+      <Feature />
     </main>
   );
 }
