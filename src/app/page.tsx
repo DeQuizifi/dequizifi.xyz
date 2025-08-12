a21c1868fec11c0ce505c6e5e138e12304b4ff6c
Hala Jeet <halajeetweb3@gmail.com>
2025-08-11 21:58:34 -0600

    Add CTA component to enhance the homepage with a call-to-action section (#22)
    Co-authored-by: Felix Jordan <felixjordan.dev@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Section/Hero/Hero";
 import Feature from "../components/Section/Feature/Feature";
+import CTA from "../components/Section/CTA/CTA";
 import About from "@/components/Section/About/About";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Hero />
       <Feature />
+      <CTA />
       <About />
     </main>
   );
