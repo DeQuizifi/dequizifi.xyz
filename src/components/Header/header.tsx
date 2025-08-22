604dbc28c65f5506585f2fbef2ad92eb1f05d7f1
Hala Jeet <halajeetweb3@gmail.com>
2025-08-22 04:08:18 -0600

    feat: Enhance Header and CTA components with improved accessibility and styling

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,26 +1,25 @@
 type HeaderProps = {
-    title: string;
-    subtitle?: string;
-}  
+  title: string;
+  subtitle?: string;
+};
 
-export default function Header({title,subtitle}: HeaderProps) {
-    return (
-        <div className="bg-sidebar-primary-foreground p-4">
-            <header>
-                <h1 className="
-                text-4xl 
-                font-bold 
-                text-center 
-                bg-gradient-to-r
-                from-ring 
-                to-popover 
-                bg-clip-text 
-                text-transparent 
-                mb-1
-  ">{title}</h1>
-                {subtitle && 
-                <p className="text-xl text-center text-chart-5">{subtitle}</p>}
-            </header>
-        </div>
-    )
+export default function Header({ title, subtitle }: HeaderProps) {
+  return (
+    <header
+      className="bg-sidebar-primary-foreground p-4"
+      aria-label="Page Header"
+    >
+      <h1 className="text-4xl font-bold text-center bg-gradient-to-r from-ring to-popover bg-clip-text text-transparent mb-1">
+        {title}
+      </h1>
+      {subtitle && (
+        <h2
+          className="text-xl text-center text-chart-5"
+          aria-label="Page Subtitle"
+        >
+          {subtitle}
+        </h2>
+      )}
+    </header>
+  );
 }
